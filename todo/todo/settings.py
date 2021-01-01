0aea6ca2607fd32553b04099081df4f79ad43d6b
ross-dotcom <wross843@gmail.com>
2021-01-01 14:40:30 +0200

    Deleted calendar.html and custom.html, changed layout of index.html page: added option to add a task. Still working on fixing JavaScript.

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
